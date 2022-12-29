dcc270a5fb39c00f3cd59e37685cdc3afc26a14d
Tracey Birch <tracey.birch@blackhorsesolutions.com>
2022-12-29 16:12:09 -0500

    connect & server info only in here

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,7 +2,7 @@ from ward import test
 
 from sliver import SliverClient
 
-from .fixtures import sliver_client
+from .fixtures import TestConstants, constants, sliver_client
 
 
 @test("Client can get version", tags=["client", "server_info", "connect"])
@@ -13,11 +13,6 @@ async def _(client: SliverClient = sliver_client):  # type: ignore
 @test("Client can list operators", tags=["client", "server_info"])
 async def _(client: SliverClient = sliver_client):  # type: ignore
     assert await client.operators()
-
-
-@test("Client can list implant builds", tags=["client", "server_info"])
-async def _(client: SliverClient = sliver_client):  # type: ignore
-    assert await client.implant_builds()
 
 
 @test("Client can list jobs", tags=["client", "server_info"])
@@ -32,5 +27,5 @@ async def _(client: SliverClient = sliver_client):  # type: ignore
 
 
 @test("Client can get job by port", tags=["client", "server_info"])
-async def _(client: SliverClient = sliver_client):  # type: ignore
-    assert await client.job_by_port(80)
+async def _(client: SliverClient = sliver_client, const: TestConstants = constants):  # type: ignore
+    assert await client.job_by_port(const.multiplayer_job_port)
