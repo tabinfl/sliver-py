6eefcf8875deeae46e71d6a8df64a8216e380115
Tracey Birch <tracey.birch@blackhorsesolutions.com>
2022-12-29 17:15:28 -0500

    beacon & session test module

--- tests/test_beacon_session.py
+++ tests/test_beacon_session.py
@@ -5,18 +5,18 @@ from sliver import SliverClient
 from .fixtures import sliver_client
 
 
-@test("Client can list beacons", tags=["client"])
+@test("Client can list beacons", tags=["client", "beacon"])
 async def _(client: SliverClient = sliver_client):  # type: ignore
     assert await client.beacons()
 
 
-@test("Client can list beacons by ID", tags=["client"])
+@test("Client can list beacons by ID", tags=["client", "beacon"])
 async def _(client: SliverClient = sliver_client):  # type: ignore
     beacons = await client.beacons()
     assert await client.beacon_by_id(beacons[0].ID)
 
 
-@test("Client can rename a beacon", tags=["client"])
+@test("Client can rename a beacon", tags=["client", "beacon"])
 async def _(client: SliverClient = sliver_client):  # type: ignore
     beacons = await client.beacons()
     beacon_name = beacons[0].Name
@@ -29,18 +29,25 @@ async def _(client: SliverClient = sliver_client):  # type: ignore
     await client.rename_beacon(beacon.ID, beacon_name)
 
 
-@test("Client can list sessions", tags=["client"])
+@test("Client can interact with a beacon", tags=["client", "beacon"])
+async def _(client: SliverClient = sliver_client):  # type: ignore
+    beacons = await client.beacons()
+    beacon = beacons[0]
+    assert await client.interact_beacon(beacon.ID)
+
+
+@test("Client can list sessions", tags=["client", "session"])
 async def _(client: SliverClient = sliver_client):  # type: ignore
     assert await client.sessions()
 
 
-@test("Client can list sessions by ID")
+@test("Client can list sessions by ID", tags=["client", "session"])
 async def _(client: SliverClient = sliver_client):  # type: ignore
     sessions = await client.sessions()
     assert await client.session_by_id(sessions[0].ID)
 
 
-@test("Client can rename a session", tags=["client"])
+@test("Client can rename a session", tags=["client", "session"])
 async def _(client: SliverClient = sliver_client):  # type: ignore
     sessions = await client.sessions()
     session_name = sessions[0].Name
@@ -53,15 +60,8 @@ async def _(client: SliverClient = sliver_client):  # type: ignore
     await client.rename_session(session.ID, session_name)
 
 
-@test("Client can interact with a session", tags=["client"])
+@test("Client can interact with a session", tags=["client", "session"])
 async def _(client: SliverClient = sliver_client):  # type: ignore
     sessions = await client.sessions()
     session = sessions[0]
     assert await client.interact_session(session.ID)
-
-
-@test("Client can interact with a beacon", tags=["client"])
-async def _(client: SliverClient = sliver_client):  # type: ignore
-    beacons = await client.beacons()
-    beacon = beacons[0]
-    assert await client.interact_beacon(beacon.ID)
